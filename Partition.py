0ebb1af9361f42ab79c2b5a6eb65dec047727d25
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-22 19:39:26 +0530

    partition

--- Partition.py
+++ Partition.py
@@ -1,1 +1,26 @@
-#Partition a linked list aroun a value of x
+#Partition a linked list around a value of x, less than x come before and greater than come after
+
+from LinkedList import LinkedList
+ 
+def partition(ll,x):
+    node=ll.head
+    ll.tail=node
+
+    while node:
+        nextNode=node.next
+        node.next=None
+        if node.value<x:
+            node.next=ll.head
+            ll.head=node
+        else:
+            ll.tail.next=node
+            ll.tail=node
+        node=nextNode
+    if ll.tail.next:
+        ll.tail.next=None
+
+ll=LinkedList()
+ll.generate(10,0,99)
+print(ll)
+partition(ll,15)
+print(ll)
