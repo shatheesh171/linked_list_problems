bd27208d81cc9e6b95670bc5456e73e9066b365c
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-22 20:15:05 +0530

    sum of lists

--- SumLists.py
+++ SumLists.py
@@ -1,30 +1,26 @@
 # Sum two linked list with numbers in reverse order and represent the sum in linked list in reverse order,
 #  each element has one digit
 
+#Time and space both are O(n) here
+
 from LinkedList import LinkedList,Node
 
 def sumList(ll1,ll2):
     n1=ll1.head
     n2=ll2.head
     ll=LinkedList()
-    ten=0
+    carry=0
     
     while n1 or n2:
-        sum=n1.value+n2.value+ten
-        #print(sum)
-        one=sum%10
-        ten=int(sum/10)
-        node=Node(one)
-        n1=n1.next
-        n2=n2.next
-        if ll.head is None: 
-            ll.head=node
-            ll.tail=node
-            continue
-        ll.tail.next=node
-        ll.tail=node
-        #print("count")
-        
+        result=carry
+        if n1:
+            result+=n1.value
+            n1=n1.next
+        if n2:
+            result+=n2.value
+            n2=n2.next
+        ll.add(result%10)
+        carry=int(result/10)
     return ll
 
 
